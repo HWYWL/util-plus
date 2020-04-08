49f9d96eadbcef3d473669883ecc03d98e979741
hwy <ilovey_hwy@163.com>
2020-04-08 17:20:18 +0800

    更新文档

--- README.md
+++ README.md
@@ -9,18 +9,18 @@
 <dependency>
   <groupId>com.github.hwywl</groupId>
   <artifactId>util-plus</artifactId>
-  <version>1.0.1-RELEASE</version>
+  <version>1.0.2-RELEASE</version>
 </dependency>
 ```
 
 **Gradle**
 ```
-implementation 'com.github.hwywl:util-plus:1.0.1-RELEASE'
+implementation 'com.github.hwywl:util-plus:1.0.2-RELEASE'
 ```
 
 
 
-### 下载
+## 下载
 以Spring boot构建的项目为例，我们查看如下代码：
 ```java
 @RestController
@@ -50,4 +50,75 @@ public class DownController {
 Writer.create().withRows(list).isAppend(true).to("aaa.txt");
 ```
 只要浏览器请求 /downFile这个接口就能下载一个名称为**悯农.txt**的文件，让其他繁琐的操作统统交给**util-plus**吧
+
+
+## 阿里云LogHub日志
+### 初始化
+在使用之前需要初始化基本的参数，如果你使用Spring Boot，可以是用@Bean注解初始化
+```
+@Bean
+public MessageResult getAccountUrl() {
+	LogHubUtil.init(
+            "cn-shanghai.log.aliyuncs.com",
+            "****************",
+            "****************",
+            "ikylin-errorlog",
+            "consumer-online",
+            "test",
+            "test"
+    );
+}
+```
+
+如果你只是一个普通的Java项目，把初始化数据写在程序执行开始的地方即可，可以参考**Test**中的案例。
+
+这就是上面所需要的参数
+![1.png](http://yanxuan.nosdn.127.net/d652eba243bc4f33ddc1a18ab4dd2c63.png)
+
+### 使用
+分为两个重载方法，一个是推送普通数据的，另一个是推送异常日志的。
+
+这样就把数据推送到日志库了：
+```
+Map<String, String> map = new HashMap<>();
+map.put("send_server_id", "11");
+map.put("receive_server_id", "1");
+map.put("chat_type", "私聊");
+map.put("open_id", "rfikaseoioire98weio3966666");
+
+PutLogsResponse putLogsResponse = LogHubService.create().pushLogHub(map);
+```
+还**支持**批量推送：
+```
+List<Map<String, String>> batch = new ArrayList<>();
+Map<String, String> map = new HashMap<>();
+map.put("send_server_id", "11");
+map.put("receive_server_id", "1");
+map.put("chat_type", "私聊");
+map.put("open_id", "rfikaseoioire98weio3966666");
+
+batch.add(map);
+
+PutLogsResponse putLogsResponse = LogHubService.create().pushLogHubBatch(batch);
+```
+
+异常推送，因为考虑到一个**try catch**为一个异常，所以**不支持**批量推送：
+```
+String text = "{\"open_id\":\"rfikaseoioire98weio3966666\",\"platform_id\":11,\"app_id\":11}";
+
+JSONObject jsonObject = JSONObject.parseObject(text);
+try {
+    Integer openId = (Integer) jsonObject.get("open_id");
+} catch (Exception e) {
+    LogHubService.create().pushLogHub(null, Loglevel.ERROR, text, e);
+    e.printStackTrace();
+}
+```
+上面代码中**pushLogHub**中有一个null的入参，其实那个是自定义参数，可传可不传，方便扩展，如果传只需要定义一个Map传进去即可。
+
+### 扩展
+如果**source**和**topic**需要自定义传，可以使用下面这种链式表达式即可，如果不传使用最开始配置中的参数。
+```
+LogHubService.create().source("app").topic("android").pushLogHub(map, Loglevel.ERROR, text, e);
+```
 
--- src/test/java/com/github/hwywl/AliTest.java
+++ src/test/java/com/github/hwywl/AliTest.java
@@ -7,7 +7,9 @@ import com.github.hwywl.service.LogHubService;
 import com.github.hwywl.utils.LogHubUtil;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -34,21 +36,31 @@ public class AliTest {
         map.put("receive_server_id", "1");
         map.put("chat_type", "私聊");
         map.put("open_id", "rfikaseoioire98weio3966666");
-        map.put("send_role_level", "1");
-        map.put("receive_role_level", "1");
-        map.put("send_vip_level", "1");
-        map.put("receive_vip_level", "1");
-        map.put("send_role_id", "role666");
-        map.put("send_role_name", "测试");
-        map.put("receive_role_id", "role777");
-        map.put("receive_role_name", "被测试");
-        map.put("event_time", (System.currentTimeMillis() / 1000) + "");
-        map.put("event_date", "2020-04-03 14:52:52");
-        map.put("chat_content", "测试数据");
-        map.put("app_id", "11");
-        map.put("platform_id", "11");
 
         PutLogsResponse putLogsResponse = LogHubService.create().pushLogHub(map);
+    }
+
+    @Test
+    public void pushBatch() {
+        LogHubUtil.init(
+                "cn-shanghai.log.aliyuncs.com",
+                "****************",
+                "******************",
+                "jk-chat",
+                "game-chat-log",
+                "test",
+                "test"
+        );
+        List<Map<String, String>> batch = new ArrayList<>();
+        Map<String, String> map = new HashMap<>();
+        map.put("send_server_id", "11");
+        map.put("receive_server_id", "1");
+        map.put("chat_type", "私聊");
+        map.put("open_id", "rfikaseoioire98weio3966666");
+
+        batch.add(map);
+
+        PutLogsResponse putLogsResponse = LogHubService.create().pushLogHubBatch(batch);
     }
 
     /**
@@ -65,7 +77,7 @@ public class AliTest {
                 "test",
                 "test"
         );
-        String text = "{\"account_type\":5,\"device_app_register_time\":1585822911,\"open_id\":\"4zbBUpcZGf0SRa011wJr10030JjVIjU\",\"role_level\":300,\"currency_balance_info\":\"[\\n  {\\n    \\\"id\\\" : 1,\\n    \\\"num\\\" : 7854862771\\n  },\\n  {\\n    \\\"id\\\" : 6,\\n    \\\"num\\\" : 41840\\n  }\\n]\",\"language\":\"zh-Hans-CN\",\"ua\":\"\",\"screen_height\":\"736\",\"role_id\":\"1000040000000039\",\"project_id\":\"26\",\"model\":\"iPhone10,2\",\"app_id\":113,\"brand\":\"Apple\",\"user_ip\":\"171.82.174.73\",\"screen_width\":\"414\",\"ios_app_version\":\"1.0\",\"server_id\":4,\"role_name\":\"萌萌\",\"distributor_id\":1,\"ios_app_build\":\"1.0\",\"role_create_time\":1585822989,\"vip_level\":11,\"channel_id\":1,\"register_device_app_register_time\":1585822911,\"server_name\":\"\",\"cp_open_id\":\"000O4zbBUpcZGf0SRa011wJr10030JjVIjU\",\"ios_idfv\":\"73AC265C-3248-4907-8986-6AA687C97651\",\"bssid\":\"\",\"event_time_server\":1585900065,\"register_channel_id\":1,\"ssid\":\"\",\"mac\":\"02:00:00:00:00:00\",\"network_info\":\"4G\",\"register_time\":1585822937,\"ios_idfa\":\"7EC652C3-95DC-4DC1-A5EF-5CE069E0720F\",\"register_device_id\":\"7EC652C3-95DC-4DC1-A5EF-5CE069E0720F\",\"device_name\":\"iPhone\",\"vip_type\":-1813697253438076400,\"account_name\":\"\",\"sdk_version\":\"1.0.5\",\"history_recharge\":128800,\"orientation\":0,\"vip_remain\":0,\"os\":2,\"account_register_app_id\":113,\"os_version\":\"12.1\",\"svip_remain\":0,\"app_register_time\":1585822937,\"app_name\":\"狂斩之刃\",\"pkg_name\":\"com.jinke.kzzr.aiqu\",\"boot_time\":\"1585310080\",\"app_key\":\"03c5517fcfc26872ac0fd22f038e371b\",\"event_id\":113,\"party_id\":0,\"platform_id\":213,\"event_time_client\":1585900065}";
+        String text = "{\"open_id\":\"rfikaseoioire98weio3966666\",\"platform_id\":11,\"app_id\":11}";
 
 
         Map<String, String> map = new HashMap<>();
@@ -77,7 +89,7 @@ public class AliTest {
         try {
             Integer openId = (Integer) jsonObject.get("open_id");
         } catch (Exception e) {
-            LogHubService.create().pushLogHub(map, Loglevel.ERROR, text, e);
+            LogHubService.create().source("app").topic("android").pushLogHub(map, Loglevel.ERROR, text, e);
             e.printStackTrace();
         }
     }
